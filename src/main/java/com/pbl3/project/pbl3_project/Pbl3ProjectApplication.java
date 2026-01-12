e08cc9bad23f716e312daae439de7a748878c067
chi kiet <macbook@MacBook-Pro-cua-Macbook.local>
2026-01-12 15:40:48 +0700

    Initial commit

---
+++ src/main/java/com/pbl3/project/pbl3_project/Pbl3ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.pbl3.project.pbl3_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pbl3ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pbl3ProjectApplication.class, args);
+	}
+
+}
